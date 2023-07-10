9b77b87a47b30ce087be303326f4d2f447483b92
Pouya Moazzezi <pmoazzezi@gmail.com>
2023-07-10 19:18:24 +0330

    added sentence-by-sentence method

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Animation Functions for use (Also copy the Required Lines in CSS File):
-const animateLetterByLetter = (animatingParagraph, delay_ms) => {
-  const animatingLetterSpanEls = animatingParagraph.innerText
+const animateLetterByLetter = (animatingElement, delay_ms) => {
+  const animatingLetterSpanEls = animatingElement.innerText
     .split("")
     .map(letter => {
       const spanElement = document.createElement("span")
@@ -9,10 +9,10 @@ const animateLetterByLetter = (animatingParagraph, delay_ms) => {
       return spanElement
     })
 
-  animatingParagraph.innerHTML = ""
+  animatingElement.innerHTML = ""
 
   animatingLetterSpanEls.forEach(element => {
-    animatingParagraph.appendChild(element)
+    animatingElement.appendChild(element)
   })
 
   animatingLetterSpanEls.forEach((element, i) => {
@@ -22,8 +22,8 @@ const animateLetterByLetter = (animatingParagraph, delay_ms) => {
   })
 }
 
-const animatingWordByWord = (animatingParagraph, delay_ms) => {
-  const animatingWordSpanEls = animatingParagraph.innerText
+const animatingWordByWord = (animatingElement, delay_ms) => {
+  const animatingWordSpanEls = animatingElement.innerText
     .split(" ")
     .map(word => {
       const spanElement = document.createElement("span")
@@ -32,10 +32,10 @@ const animatingWordByWord = (animatingParagraph, delay_ms) => {
       return spanElement
     })
 
-  animatingParagraph.innerHTML = ""
+  animatingElement.innerHTML = ""
 
   animatingWordSpanEls.forEach(element => {
-    animatingParagraph.appendChild(element)
+    animatingElement.appendChild(element)
   })
 
   animatingWordSpanEls.forEach((element, i) => {
@@ -45,12 +45,37 @@ const animatingWordByWord = (animatingParagraph, delay_ms) => {
   })
 }
 
+const animatingSentenceBySentence = (animatingElement, delay_ms) => {
+  const animatingSentenceSpanEls = animatingElement.innerText
+    .split(".")
+    .map(word => {
+      const spanElement = document.createElement("span")
+      spanElement.innerText = word + "."
+
+      return spanElement
+    })
+
+  animatingElement.innerHTML = ""
+
+  animatingSentenceSpanEls.forEach(element => {
+    animatingElement.appendChild(element)
+  })
+
+  animatingSentenceSpanEls.forEach((element, i) => {
+    setTimeout(() => {
+      element.classList.add("animating-sentence")
+    }, (i + 1) * delay_ms)
+  })
+}
+
 // Example App Config:
-const LETTER_BY_LETTER_ANIMATION_DELAY_MS = 30
+const LETTER_BY_LETTER_ANIMATION_DELAY_MS = 10
 const WORD_BY_WORD_ANIMATION_DELAY_MS = 400
+const SENTENCE_BY_SENTENCE_DELAY_MS = 1000
 const ANIMATING_METHODS = {
   WORD_BY_WORD: "word-by-word",
-  LETTER_BY_LETTER: "letter-by-letter"
+  LETTER_BY_LETTER: "letter-by-letter",
+  SENTENCE_BY_SENTENCE: "sentence-by-sentence"
 }
 
 // Example App:
@@ -68,6 +93,12 @@ switch (animatingMethod) {
   case ANIMATING_METHODS.WORD_BY_WORD:
     animatingWordByWord(animatingParagraph, WORD_BY_WORD_ANIMATION_DELAY_MS)
     break
+
+  case ANIMATING_METHODS.SENTENCE_BY_SENTENCE:
+    animatingSentenceBySentence(
+      animatingParagraph,
+      SENTENCE_BY_SENTENCE_DELAY_MS
+    )
 
   default:
     break
