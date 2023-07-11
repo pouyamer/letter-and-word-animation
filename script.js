3c6c5a8dd6ff7cd6f4a0fc5c050090b027dbc44f
Pouya Moazzezi <pmoazzezi@gmail.com>
2023-07-12 01:32:12 +0330

    generalization of some terms like element etc

--- script.js
+++ script.js
@@ -1,104 +1,123 @@
 // Animation Functions for use (Also copy the Required Lines in CSS File):
-const animateLetterByLetter = (animatingElement, delay_ms) => {
-  const animatingLetterSpanEls = animatingElement.innerText
-    .split("")
-    .map(letter => {
-      const spanElement = document.createElement("span")
-      spanElement.innerText = letter
+const applyStyleLetterByLetter = (element, delay_ms) => {
+  const styledLetterSpanEls = element.innerText.split("").map(letter => {
+    const span = document.createElement("span")
+    span.innerText = letter
 
-      return spanElement
-    })
-
-  animatingElement.innerHTML = ""
-
-  animatingLetterSpanEls.forEach(element => {
-    animatingElement.appendChild(element)
+    return span
   })
 
-  animatingLetterSpanEls.forEach((element, i) => {
+  element.innerHTML = ""
+
+  styledLetterSpanEls.forEach(span => {
+    element.appendChild(span)
+  })
+
+  styledLetterSpanEls.forEach((span, i) => {
     setTimeout(() => {
-      element.classList.add("animating-letter")
+      span.classList.add("styled-letter")
     }, (i + 1) * delay_ms)
   })
 }
 
-const animatingWordByWord = (animatingElement, delay_ms) => {
-  const animatingWordSpanEls = animatingElement.innerText
-    .split(" ")
-    .map(word => {
-      const spanElement = document.createElement("span")
-      spanElement.innerText = word + " "
+const applyStyleWordByWord = (
+  element,
+  delay_ms,
+  contentAware = false,
+  maxWordLengthForFullDelay = 10,
+  isDelayAppliedRandomly = false
+) => {
+  const styledWordSpanEls = element.innerText.split(" ").map(word => {
+    const span = document.createElement("span")
 
-      return spanElement
-    })
+    span.innerHTML = word + " "
 
-  animatingElement.innerHTML = ""
-
-  animatingWordSpanEls.forEach(element => {
-    animatingElement.appendChild(element)
+    return span
   })
 
-  animatingWordSpanEls.forEach((element, i) => {
+  element.innerHTML = ""
+
+  styledWordSpanEls.forEach(span => {
+    element.appendChild(span)
+  })
+
+  let accumulatedDelayForContentAwareMode = 0
+  let delaysforContentAwareMode = styledWordSpanEls.map((span, i) => {
+    accumulatedDelayForContentAwareMode +=
+      ((span.innerText.length - 1) / maxWordLengthForFullDelay) * delay_ms
+
+    return accumulatedDelayForContentAwareMode
+  })
+
+  styledWordSpanEls.forEach((span, i) => {
+    const calculateDelay = () => {
+      if (contentAware) return delaysforContentAwareMode[i]
+      if (isDelayAppliedRandomly)
+        return (i + 1) * Math.random() * delay_ms(i + 1) * delay_ms
+      return (i + 1) * delay_ms
+    }
+
     setTimeout(() => {
-      element.classList.add("animating-word")
-    }, (i + 1) * delay_ms)
+      span.classList.add("styled-word")
+    }, calculateDelay())
   })
 }
 
-const animatingSentenceBySentence = (animatingElement, delay_ms) => {
-  const animatingSentenceSpanEls = animatingElement.innerText
-    .split(".")
-    .map(word => {
-      const spanElement = document.createElement("span")
-      spanElement.innerText = word + "."
+const applyStyleSentenceBySentence = (element, delay_ms) => {
+  const styledSentenceSpanEls = element.innerText.split(".").map(word => {
+    const span = document.createElement("span")
+    span.innerText = word + "."
 
-      return spanElement
-    })
-
-  animatingElement.innerHTML = ""
-
-  animatingSentenceSpanEls.forEach(element => {
-    animatingElement.appendChild(element)
+    return span
   })
 
-  animatingSentenceSpanEls.forEach((element, i) => {
+  element.innerHTML = ""
+
+  styledSentenceSpanEls.forEach(span => {
+    element.appendChild(span)
+  })
+
+  styledSentenceSpanEls.forEach((span, i) => {
     setTimeout(() => {
-      element.classList.add("animating-sentence")
+      span.classList.add("styled-sentence")
     }, (i + 1) * delay_ms)
   })
 }
 
 // Example App Config:
-const LETTER_BY_LETTER_ANIMATION_DELAY_MS = 10
-const WORD_BY_WORD_ANIMATION_DELAY_MS = 400
-const SENTENCE_BY_SENTENCE_DELAY_MS = 1000
-const ANIMATING_METHODS = {
+const LETTER_BY_LETTER_STYLING_DELAY_MS = 120
+const WORD_BY_WORD_STYLING_DELAY_MS = 150
+const SENTENCE_BY_SENTENCE_STYLING_DELAY_MS = 1000
+const STYLING_METHODS = {
   WORD_BY_WORD: "word-by-word",
   LETTER_BY_LETTER: "letter-by-letter",
   SENTENCE_BY_SENTENCE: "sentence-by-sentence"
 }
 
 // Example App:
-const animatingMethod = ANIMATING_METHODS.LETTER_BY_LETTER
-const animatingParagraph = document.querySelector(".animating-paragraph")
+const stylingMethod = STYLING_METHODS.WORD_BY_WORD
+const styledElement = document.querySelector(".styled-element")
 
-switch (animatingMethod) {
-  case ANIMATING_METHODS.LETTER_BY_LETTER:
-    animateLetterByLetter(
-      animatingParagraph,
-      LETTER_BY_LETTER_ANIMATION_DELAY_MS
-    )
+switch (stylingMethod) {
+  case STYLING_METHODS.LETTER_BY_LETTER:
+    applyStyleLetterByLetter(styledElement, LETTER_BY_LETTER_STYLING_DELAY_MS)
     break
   // Word-by-word Method
-  case ANIMATING_METHODS.WORD_BY_WORD:
-    animatingWordByWord(animatingParagraph, WORD_BY_WORD_ANIMATION_DELAY_MS)
+  case STYLING_METHODS.WORD_BY_WORD:
+    applyStyleWordByWord(
+      styledElement,
+      WORD_BY_WORD_STYLING_DELAY_MS,
+      (contentAware = true),
+      3
+    )
     break
 
-  case ANIMATING_METHODS.SENTENCE_BY_SENTENCE:
-    animatingSentenceBySentence(
-      animatingParagraph,
-      SENTENCE_BY_SENTENCE_DELAY_MS
+  case STYLING_METHODS.SENTENCE_BY_SENTENCE:
+    applyStyleSentenceBySentence(
+      styledElement,
+      SENTENCE_BY_SENTENCE_STYLING_DELAY_MS
     )
+    break
 
   default:
     break
